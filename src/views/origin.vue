9f81f5757a51f9fd04134f3f4c3d4c0c8fe600cb
cuizhenhao <1278493070@qq.com>
2022-12-16 13:20:08 +0800

    溯源查询

--- src/views/origin.vue
+++ src/views/origin.vue
@@ -15,13 +15,14 @@
         style="width: 100%;">
       <el-table-column label="电影编号" prop="asin">
         <template v-slot="scope">
-          <a :href="'https:www.amazon.com/dp/'+scope.row.asin" target="_blank" class="buttonText">{{scope.row.asin}}</a>
+          <a :href="'https:www.amazon.com/dp/'+scope.row.asin" target="_blank"
+             class="buttonText">{{ scope.row.asin }}</a>
         </template>
       </el-table-column>
-      <el-table-column label="电影名称" prop="name"/>
+      <el-table-column label="电影名称" prop="title"/>
       <el-table-column label="电影得分" prop="score"/>
       <el-table-column label="导演" prop="director"/>
-      <el-table-column label="演员" prop="actor"/>
+      <el-table-column label="演员" prop="actors"/>
       <el-table-column label="电影类别" prop="genre"/>
       <el-table-column label="发布时间" prop="releaseTime"/>
       <el-table-column label="评论数量" prop="commentNum"/>
@@ -31,50 +32,52 @@
         style="width:40vh; margin-bottom:10px"
     >
     </el-input>
-    <el-button icon="el-icon-search" circle style="margin-top:-10px; margin-left:10px" @click="searchMovieByTitle"></el-button>
+    <el-button icon="el-icon-search" circle style="margin-top:-10px; margin-left:10px"
+               @click="searchMovieByTitle"></el-button>
   </div>
 </template>
 
 <script>
 import axios from "axios";
+import request from "@/utils/requests";
 
 export default {
   name: "origin",
   data() {
     return {
-      BASE_URL: 'http://localhost:8888',
+      BASE_URL: 'http://localhost:8084',
       dialogData: {
-        movieTitle: 'hello',
-        versionCount: 4,
-        commentCount: 3,
+        movieTitle: "",
+        versionCount: 0,
+        commentCount: 0,
       },
       searchingTitle: '',
       movieData: [
         {
-          asin: 1,
-          name: 1,
-          score: 1,
-          director: 1,
-          actor: 1,
-          genre: 1,
-          releaseTime: 1,
-          commentNum: 1
+          asin: "",
+          title: "",
+          score: "",
+          director: "",
+          actors: "",
+          genre: "",
+          releaseTime: "",
+          commentNum: ""
         }
       ]
     }
   },
   methods: {
-    searchMovieByTitle () {
-      var axios = require('axios');
-      // 向mysql发送请求
-      axios({
-        method: 'get',
-        url: this.BASE_URL + '',
-        data: this.searchingTitle,
-        headers: {}
-      }).then(response => {
-        this.dialogData = response.dialogs
-        this.movieData = response.movies
+    searchMovieByTitle() {
+      request.get("/movie/mysql/getOrigin", {
+        params: {"searchingTitle": this.searchingTitle}
+      }).then(res => {
+        this.dialogData.commentCount = 0
+        this.movieData = res.movies
+        this.dialogData.versionCount = res.movies.length
+        this.dialogData.movieTitle = this.searchingTitle
+        for (let i = 0; i < res.movies.length; i++) {
+          this.dialogData.commentCount += this.movieData[i].commentNum
+        }
       })
     }
   }
@@ -87,10 +90,12 @@ export default {
   top: 500px;
   left: -740px;
 }
+
 .el-table {
   margin-top: 20px;
   margin-bottom: 20px;
 }
+
 a {
   text-decoration: none;
   color: black;
--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = {
     port: 8081,
     proxy: {                 //设置代理，必须填
       '/api': {              //设置拦截器  拦截器格式   斜杠+拦截器名字，名字可以自己定
-        target: 'http://localhost:8443',     //代理的目标地址
+        target: 'http://localhost:8084',     //代理的目标地址
         changeOrigin: true,              //是否设置同源，输入是的
         pathRewrite: {                   //路径重写
                      //选择忽略拦截器里面的内容
